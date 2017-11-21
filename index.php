0dae8ad59dd0da087f2a580625de2cfd8f9dff3b
Mohammad Abdollahi <dr.m.abdollahi@gmail.com>
2017-11-22 02:34:12 +0330

    Add files via upload

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+
+
+class hadse_horoof
+{
+    private $number;
+    private $verb;
+    private $pluralModifier;
+
+    public function make($candidate, $count)
+    {
+        $this->MasterMethod($count);
+        return sprintf(
+            'String %s %s %s%s', $this->verb, $this->number, $candidate, $this->pluralModifier
+        );
+    }
+
+    private function MasterMethod($count)
+    {
+        if ($count == 0) {
+            $this->bedoone_harf();
+        } elseif ($count == 1) {
+            $this->yek_harf();
+        } else {
+            $this->more_harf($count);
+        }
+    }
+
+    private function more_harf($count)
+    {
+        $this->number = strval($count);
+        $this->verb   = 'are';
+        $this->pluralModifier = 's';
+    }
+
+    private function yek_harf()
+    {
+        $this->number = '1';
+        $this->verb   = 'is';
+        $this->pluralModifier = '';
+    }
+
+    private function bedoone_harf()
+    {
+        $this->number = 'no';
+        $this->verb   = 'are';
+        $this->pluralModifier = 's';
+    }
+}
+?>
